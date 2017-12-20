bb2880627b3bb27550c96a720f046ff253568ed4
Ahmad Barqawi <Barqawi.88@outlook.com>
2017-12-21 00:09:55 +0300

    beta version 0.1

--- README.md
+++ README.md
@@ -1,21 +1,65 @@
-# Tshkel Project مشروع تشكيل
+# Shakkala Project V 0.1 مشروع شكّالة
 ## Introduction
-..
+Shakkala project use recurrent neural network to automatically form Arabic characters (تشكيل الحروف).<br/>
+This is beta version 0.1
+
 ## Requirements
-..
+Execute following commands:<br/>
+```
+cd requirements
+pip install -r requirements.txt
+```
 ## Code Examples (How to)
-..
+Check full example in (demo.py) file.<br/>
+
+1. Create Shakkala object
+```
+sh = Shakkala(folder_location)
+```
+2. Prepare input
+```
+input_int = sh.prepare_input(input_text)
+```
+3. Call the neural network
+```
+model, graph = sh.get_model()
+with graph.as_default():
+      logits = model.predict(input_int)[0]
+```
+4. Predict output
+```
+predicted_harakat = sh.logits_to_text(logits)
+final_output = sh.get_final_text(input_text, predicted_harakat)
+```
+
+## Accuracy
+This beta version trained with personal computer on historical Arabic data and reached accuracy of almost 90%.
+
+### Lose History
+<img src="images/loss_history.png" alt="Loss History" style="height: 100px;"/>
+
+### Prediction Example
+- Real output :<br/>
+فَإِنْ لَمْ يَكُونَا كَذَلِكَ أَتَى بِمَا يَقْتَضِيهِ الْحَالُ وَهَذَا أَوْلَى
+<br/>
+- Predicted output :<br/>
+فَإِنْ لَمْ يَكُونُا كَذَلِكَ أَتَى بِمَا يَقْتَضِيهِ الْحَالُ وَهُذَا أَوْلَى
+
+### Accuracy Enhancements  
+The model can be enhanced to reach 95%-98% with following:<br/>
+- Availability of model formed data to train the network. (because current version trained with the available historical Arabic data only)
+- Rent a server with high GPU to increase the number of epochs, layer and neural units.
+
 ## Team
-1. Ahmad Barqawi <br/>
-2. Moayyad Yaghi <br/>
-3. Taha Zerrouki <br/>
+1. Ahmad Barqawi: Neural Network Developer.<br/>
+2. Taha Zerrouki: Mentor Data and Results.<br/>
 
 
 License
 -------
     The MIT License (MIT)
 
-    Copyright (c) 2017 Tashkel Project
+    Copyright (c) 2017 Shakkala Project
 
     Permission is hereby granted, free of charge, to any person obtaining a copy
     of this software and associated documentation files (the "Software"), to deal
---
+++ Shakkala.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+License
+-------
+    The MIT License (MIT)
+
+    Copyright (c) 2017 Tashkel Project
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+Created on Sat Dec 16 22:46:28 2017
+
+@author: Ahmad Barqawi
+"""
+import helper
+import os
+import tensorflow as tf
+from keras.models import Model
+from keras.models import load_model
+from keras.optimizers import Adam
+from keras.losses import sparse_categorical_crossentropy
+from keras.preprocessing.sequence import pad_sequences
+import numpy as np
+
+
+class Shakkala:
+
+    # intial
+    max_sentence = 495
+
+    def __init__(self, folder_location):
+
+        assert folder_location != None, "model_location cant be empty, send location of keras model"
+
+        model_folder = os.path.join(folder_location, 'model')
+        self.model_location = os.path.join(model_folder, ('simple_model' + '.h5'))
+
+        dictionary_folder = os.path.join(folder_location, 'dictionary')
+        input_vocab_to_int  = helper.load_binary('input_vocab_to_int',dictionary_folder)
+        output_int_to_vocab = helper.load_binary('output_int_to_vocab',dictionary_folder)
+
+        self.dictionary = {
+                "input_vocab_to_int":input_vocab_to_int,
+                "output_int_to_vocab":output_int_to_vocab}
+
+    # model
+    def get_model(self):
+        model = load_model(self.model_location)
+        graph = tf.get_default_graph()
+
+        return model, graph
+
+    # input processing
+
+    def prepare_input(self, input_sent):
+
+        assert input_sent != None and len(input_sent) < self.max_sentence, \
+        "max length for input_sent should be 500 characters, you can split the sentence into multiple sentecens and call the function"
+
+        input_sent = [input_sent]
+
+        return self.__preprocess(input_sent)
+
+    def __preprocess(self, input_sent):
+
+        input_vocab_to_int = self.dictionary["input_vocab_to_int"]
+
+        input_letters_ids  = [[input_vocab_to_int.get(ch, input_vocab_to_int['<UNK>']) for ch in sent] for sent in input_sent]
+
+        input_letters_ids  = self.__pad_size(input_letters_ids, self.max_sentence)
+
+        return input_letters_ids
+
+    # output processing
+
+    def logits_to_text(self, logits):
+        text = "".join(self.dictionary['output_int_to_vocab'][prediction] for prediction in np.argmax(logits,1))
+        return text.replace('<PAD>','')
+
+    def get_final_text(self,input_sent, output_sent):
+        return helper.combine_text_with_harakat(input_sent, output_sent)
+
+    def clean_harakat(self, input_sent):
+        return helper.clear_tashkel(input_sent)
+
+    # common
+    def __pad_size(self, x, length=None):
+        return pad_sequences(x, maxlen=length, padding='post')
