e216133d62d4cea15a7f2fc81358be4eb3afd35d
Ali Fadel <aliosm1997@gmail.com>
2019-02-21 21:36:34 +0200

    Fix indentation conflicts

--- Shakkala.py
+++ Shakkala.py
@@ -21,7 +21,6 @@ Created on Sat Dec 16 22:46:28 2017
 
 @author: Ahmad Barqawi
 """
-
 import helper
 import os
 import tensorflow as tf
@@ -32,77 +31,81 @@ from keras.losses import sparse_categorical_crossentropy
 from keras.preprocessing.sequence import pad_sequences
 import numpy as np
 
+
 class Shakkala:
-  # intial
-  # max_sentence = 495
 
-  def __init__(self, folder_location, version=3):
-    assert folder_location != None, "model_location cant be empty, send location of keras model"
+    # intial
+    #max_sentence = 495
 
-    model_folder = os.path.join(folder_location, 'model')
-    if version == 1:
-      self.max_sentence = 495
-      self.model_location = os.path.join(model_folder, ('simple_model' + '.h5'))
-    elif version == 2:
-      self.max_sentence = 315
-      self.model_location = os.path.join(model_folder, ('middle_model' + '.h5'))
-    elif version == 3:
-      self.max_sentence = 315
-      self.model_location = os.path.join(model_folder, ('second_model6' + '.h5'))
+    def __init__(self, folder_location, version=3):
 
-    dictionary_folder = os.path.join(folder_location, 'dictionary')
-    input_vocab_to_int  = helper.load_binary('input_vocab_to_int',dictionary_folder)
-    output_int_to_vocab = helper.load_binary('output_int_to_vocab',dictionary_folder)
+        assert folder_location != None, "model_location cant be empty, send location of keras model"
 
-    self.dictionary = {
-      "input_vocab_to_int":input_vocab_to_int,
-      "output_int_to_vocab":output_int_to_vocab
-    }
+        model_folder = os.path.join(folder_location, 'model')
+        if version == 1:
+            self.max_sentence = 495
+            self.model_location = os.path.join(model_folder, ('simple_model' + '.h5'))
+        elif version == 2:
+            self.max_sentence = 315
+            self.model_location = os.path.join(model_folder, ('middle_model' + '.h5'))
+        elif version == 3:
+            self.max_sentence = 315
+            self.model_location = os.path.join(model_folder, ('second_model6' + '.h5'))
 
-  # model
-  def get_model(self):
-    print('start load model')
-    model = load_model(self.model_location)
-    print('end load model')
-    graph = tf.get_default_graph()
+        dictionary_folder = os.path.join(folder_location, 'dictionary')
+        input_vocab_to_int  = helper.load_binary('input_vocab_to_int',dictionary_folder)
+        output_int_to_vocab = helper.load_binary('output_int_to_vocab',dictionary_folder)
 
-    return model, graph
+        self.dictionary = {
+                "input_vocab_to_int":input_vocab_to_int,
+                "output_int_to_vocab":output_int_to_vocab}
 
-  # input processing
+    # model
+    def get_model(self):
+        print('start load model')
+        model = load_model(self.model_location)
+        print('end load model')
+        graph = tf.get_default_graph()
 
-  def prepare_input(self, input_sent):
-    assert input_sent != None and len(input_sent) < self.max_sentence, \
-    "max length for input_sent should be {} characters, you can split the sentence into multiple sentecens and call the function".format(self.max_sentence)
+        return model, graph
 
-    input_sent = [input_sent]
+    # input processing
 
-    return self.__preprocess(input_sent)
+    def prepare_input(self, input_sent):
 
-  def __preprocess(self, input_sent):
-    input_vocab_to_int = self.dictionary["input_vocab_to_int"]
+        assert input_sent != None and len(input_sent) < self.max_sentence, \
+        "max length for input_sent should be {} characters, you can split the sentence into multiple sentecens and call the function".format(self.max_sentence)
 
-    input_letters_ids  = [[input_vocab_to_int.get(ch, input_vocab_to_int['<UNK>']) for ch in sent] for sent in input_sent]
+        input_sent = [input_sent]
 
-    input_letters_ids  = self.__pad_size(input_letters_ids, self.max_sentence)
+        return self.__preprocess(input_sent)
 
-    return input_letters_ids
+    def __preprocess(self, input_sent):
 
-  # output processing
+        input_vocab_to_int = self.dictionary["input_vocab_to_int"]
 
-  def logits_to_text(self, logits):
-    text = []
-    for prediction in np.argmax(logits,1):
-      if self.dictionary['output_int_to_vocab'][prediction] == '<PAD>':
-        continue
-      text.append(self.dictionary['output_int_to_vocab'][prediction])
-    return text
+        input_letters_ids  = [[input_vocab_to_int.get(ch, input_vocab_to_int['<UNK>']) for ch in sent] for sent in input_sent]
 
-  def get_final_text(self,input_sent, output_sent):
-    return helper.combine_text_with_harakat(input_sent, output_sent)
+        input_letters_ids  = self.__pad_size(input_letters_ids, self.max_sentence)
 
-  def clean_harakat(self, input_sent):
-    return helper.clear_tashkel(input_sent)
+        return input_letters_ids
 
-  # common
-  def __pad_size(self, x, length=None):
-    return pad_sequences(x, maxlen=length, padding='post')
+    # output processing
+
+    def logits_to_text(self, logits):
+        text = []
+        for prediction in np.argmax(logits, 1):
+            if self.dictionary['output_int_to_vocab'][prediction] == '<PAD>':
+                continue
+            text.append(self.dictionary['output_int_to_vocab'][prediction])
+        return text
+
+    def get_final_text(self,input_sent, output_sent):
+        return helper.combine_text_with_harakat(input_sent, output_sent)
+
+    def clean_harakat(self, input_sent):
+        return helper.clear_tashkel(input_sent)
+
+    # common
+    def __pad_size(self, x, length=None):
+        return pad_sequences(x, maxlen=length, padding='post')
