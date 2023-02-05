540d2f5da30b7ef255fe278a36e96be1f36f4f2b
Ahmad Barqawi <Barqawi.88@outlook.com>
2023-02-05 18:06:07 +0000

    try to fix pip issue

--- setup.py
+++ setup.py
@@ -2,9 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='shakkala',
-    version='0.2',
+    version='0.6',
     packages=find_packages(),
     include_package_data=True,
+    description="Deep learning for Arabic text Vocalization - التشكيل الالي للنصوص العربية",
+    long_description="The Shakkala project presents a recurrent neural network for Arabic text vocalization that automatically forms Arabic characters (تشكيل الحروف) to enhance text-to-speech systems.",
     install_requires=[
         'tensorflow==2.9.3',
         'h5py==3.8.0',
